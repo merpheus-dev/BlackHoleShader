c4f332c1c7ae861a68bf6fd7460a5176d92896ab
m3rt32 <mertkirimgeri@gmail.com>
2019-05-12 07:35:59 +0300

    Initial source

---
+++ RotateAroundBlackHole.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundBlackHole : MonoBehaviour
+{
+    [SerializeField]
+    private Transform Target;
+
+    [SerializeField]
+    private float Speed=10f;
+
+    protected void Update()
+    {
+        transform.RotateAround(Target.position,Vector3.up,Time.deltaTime*Speed);
+    }
+}
